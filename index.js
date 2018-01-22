6187c1a75f036e61ed163f3b1196955b8d68bcfa
Jonathan New <jonnew@gmail.com>
2018-01-22 10:47:59 -0800

    Server that returns a 200

---
+++ index.js
@@ -0,0 +1,15 @@
+var nconf = require("nconf"),
+	express = require('express'),
+	app = express();
+
+nconf.env().file({file: ".env"});
+
+var port = nconf.get("PORT");
+
+app.get('/', function(req, res) {
+	res.status(200).end();
+});
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});	
